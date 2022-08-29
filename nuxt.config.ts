5d7d274eef366494806d3c3231212b467f546bc5
Jeroen van Beek <losbeekos@gmail.com>
2022-08-29 19:29:30 +0200

    fix: remove theme_color so it's based on devise dark/light mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,6 @@ export default defineNuxtConfig({
       name: 'F1 Calendar',
       description:
         'F1 race weekend calendar with times in your local timezone.',
-      theme_color: '#ffffff',
     },
     workbox: {
       // TODO: Caching for F1 API in service worker.
